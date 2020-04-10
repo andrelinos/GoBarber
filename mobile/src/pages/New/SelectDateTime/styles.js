6c6bd652de780b68d5042c66ca6c656b6d634187
Andrelino Silva <andrelinocom@gmail.com>
2020-04-09 22:35:27 -0300

    Adicionando agendamentos

--- mobile/src/pages/New/SelectDateTime/styles.js
+++ mobile/src/pages/New/SelectDateTime/styles.js
@@ -1,3 +1,28 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View``;
+
+export const HourList = styled.FlatList.attrs({
+  numColumns: 2,
+  showsVerticalScrollIndicator: false,
+})`
+  padding: 20px;
+`;
+
+export const Hour = styled(RectButton)`
+  background: #fff;
+  border-radius: 5px;
+  padding: 20px;
+  flex: 1;
+  opacity: ${(props) => (props.enable ? 1 : 0.6)};
+
+  align-items: center;
+  margin: 0 10px 20px;
+`;
+
+export const Title = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #333;
+`;
